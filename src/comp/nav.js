d244470cb78e3ae7b7df815d3986f7e2f5be707c
ajayverma7398 <ajayverma906906@gmail.com>
2025-05-13 12:48:19 +0530

    3rd commit

--- src/comp/nav.js
+++ src/comp/nav.js
@@ -1,8 +1,13 @@
 import React from "react";
 import "./nav.css";
 import { MdLocalShipping } from "react-icons/md";
+import { AiOutlineSearch } from "react-icons/ai";
+import { FiLogIn } from "react-icons/fi";
+import { CiLogout } from "react-icons/ci";
+import { useAuth0 } from "@auth0/auth0-react";
 
 const Nav = () => {
+  const { loginWithRedirect } = useAuth0();
   return (
     <>
       <div className="header">
@@ -15,13 +20,31 @@ const Nav = () => {
           </div>
         </div>
         <div className="mid_header">
-            <div className="logo">
-                <img src="image/logo.webp" alt="logo"></img>
+          <div className="logo">
+            <img src="image/logo.webp" alt="logo"></img>
+          </div>
+          <div className="search_box">
+            <input type="text" value="" placeholder="search"></input>
+            <button>
+              <AiOutlineSearch />
+            </button>
+          </div>
+          <div className="user">
+            <div className="icon">
+              <FiLogIn />
             </div>
-            <div className="search_box">
-                <input type="text" value="" placeholder="search"></input>
-                <input></input>
+            <div className="btn">
+              <button onClick={() => loginWithRedirect()}>Login</button>
             </div>
+          </div>
+          <div className="user">
+            <div className="icon">
+              <CiLogout />
+            </div>
+            <div className="btn">
+              <button onClick={() => loginWithRedirect()}>Logout</button>
+            </div>
+          </div>
         </div>
       </div>
     </>
--- src/index.js
+++ src/index.js
@@ -2,12 +2,22 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
+import { Auth0Provider } from "@auth0/auth0-react";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <React.StrictMode>
+  // <React.StrictMode>
+  //   <App />
+  // </React.StrictMode>
+  <Auth0Provider
+    domain="dev-7avdijy7oqbdx2ea.us.auth0.com"
+    clientId="eJXKaCSxef9LnAli2WzzL4JFjWbERhei"
+    authorizationParams={{
+      redirect_uri: window.location.origin,
+    }}
+  >
     <App />
-  </React.StrictMode>
+  </Auth0Provider>
 );
 
 // If you want to start measuring performance in your app, pass a function
