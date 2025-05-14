73802f5275c54652e3034526b9138b0502e197ad
ajayverma7398 <ajayverma906906@gmail.com>
2025-05-15 02:54:34 +0530

    4th commit

--- src/comp/nav.js
+++ src/comp/nav.js
@@ -1,13 +1,14 @@
 import React from "react";
-import "./nav.css";
 import { MdLocalShipping } from "react-icons/md";
 import { AiOutlineSearch } from "react-icons/ai";
 import { FiLogIn } from "react-icons/fi";
-import { CiLogout } from "react-icons/ci";
+import { CiLogout, CiUser } from "react-icons/ci";
 import { useAuth0 } from "@auth0/auth0-react";
+import { Link } from "react-router-dom";
+import "./nav.css";
 
 const Nav = () => {
-  const { loginWithRedirect } = useAuth0();
+  const { loginWithRedirect, logout, user, isAuthenticated } = useAuth0();
   return (
     <>
       <div className="header">
@@ -29,21 +30,93 @@ const Nav = () => {
               <AiOutlineSearch />
             </button>
           </div>
-          <div className="user">
-            <div className="icon">
-              <FiLogIn />
+          {isAuthenticated ? (
+            // If User is Login then Logout Button will show
+            <div className="user">
+              <div className="icon">
+                <CiLogout />
+              </div>
+              <div className="btn">
+                <button
+                  onClick={() =>
+                    logout({
+                      logoutParams: { returnTo: window.location.origin },
+                    })
+                  }
+                >
+                  Logout
+                </button>
+              </div>
             </div>
-            <div className="btn">
-              <button onClick={() => loginWithRedirect()}>Login</button>
+          ) : (
+            // If User is not Login then Login Button will show
+            <div className="user">
+              <div className="icon">
+                <FiLogIn />
+              </div>
+              <div className="btn">
+                <button onClick={() => loginWithRedirect()}>Login</button>
+              </div>
             </div>
+          )}
+        </div>
+        <div className="last_header">
+          <div className="user_profile">
+            {
+              // User Profile will be Shown Here
+              isAuthenticated ? (
+                <>
+                  <div className="icon">
+                    <CiUser />
+                  </div>
+                  <div className="info">
+                    <h2>{user.name}</h2>
+                    <p>{user.email}</p>
+                  </div>
+                </>
+              ) : (
+                <>
+                  <div className="icon">
+                    <CiUser />
+                  </div>
+                  <div className="info">
+                    <p>Please Login</p>
+                  </div>
+                </>
+              )
+            }
           </div>
-          <div className="user">
-            <div className="icon">
-              <CiLogout />
-            </div>
-            <div className="btn">
-              <button onClick={() => loginWithRedirect()}>Logout</button>
-            </div>
+          <div className="nav">
+            <ul>
+              <li>
+                <Link to="/" className="link">
+                  Home
+                </Link>
+              </li>
+              <li>
+                <Link to="/shop" className="link">
+                  Shop
+                </Link>
+              </li>
+              <li>
+                <Link to="/collection" className="link">
+                  Collection
+                </Link>
+              </li>
+              <li>
+                <Link to="/about" className="link">
+                  About
+                </Link>
+              </li>
+              <li>
+                <Link to="/contact" className="link">
+                  Contact
+                </Link>
+              </li>
+            </ul>
+          </div>
+          <div className="offer">
+            <p>flat 10% off over all iphone</p>
           </div>
         </div>
       </div>
